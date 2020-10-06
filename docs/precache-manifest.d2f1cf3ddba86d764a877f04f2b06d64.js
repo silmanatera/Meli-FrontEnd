65348393df598f58487474e9747d5681f6fc5740
Silma Natera <snatera@serv.externos.prismamp.com>
2020-10-06 10:33:47 -0300

    Deploy

---
+++ docs/precache-manifest.d2f1cf3ddba86d764a877f04f2b06d64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9465e2445afc32b2d17771c1af9278b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7d5898a6e29fc46ccb",
+    "url": "/static/css/main.111e0e94.chunk.css"
+  },
+  {
+    "revision": "c7cd6e418aa0c35248dc",
+    "url": "/static/js/2.1af6fbd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1af6fbd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7d5898a6e29fc46ccb",
+    "url": "/static/js/main.d6273d78.chunk.js"
+  },
+  {
+    "revision": "846b7e5a8b8bebbb5d8b",
+    "url": "/static/js/runtime-main.3ea9f686.js"
+  }
+]);
